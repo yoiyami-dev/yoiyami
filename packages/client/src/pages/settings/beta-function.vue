843802221410522b08c61ab3e1403c5770586b96
r-ca <nem@nem.x0.to>
2023-01-26 13:41:33 +0900

    うごいた

--- packages/client/src/pages/settings/beta-function.vue
+++ packages/client/src/pages/settings/beta-function.vue
@@ -2,13 +2,19 @@
 	<div class="_formRoot">
 		<FormInfo class="_formBlock">以下の機能はすべてベータ版です。</FormInfo>
 
-		<FormSwitch v-model="$store.state.navBarChatIcon" class="_formBlock">チャットを表示する</FormSwitch>
-		<FormSwitch v-model="$store.state.navBarReloadIcon" class="_formBlock">リロードを表示する</FormSwitch>
-		<FormSwitch v-model="$store.state.navBarWidgetIcon" class="_formBlock">ウィジェットを表示する</FormSwitch>
+		<FormSwitch v-model="navBarChatIcon" class="_formBlock">チャットを表示する</FormSwitch>
+		<FormSwitch v-model="navBarReloadIcon" class="_formBlock">リロードを表示する</FormSwitch>
+		<FormSwitch v-model="navBarWidgetIcon" class="_formBlock">ウィジェットを表示する</FormSwitch>
 	</div>
 </template>
 	
 <script lang="ts" setup>
+import { computed, ref, watch } from 'vue';
+import { defaultStore } from '@/store';
 	import FormInfo from '@/components/MkInfo.vue';
 	import FormSwitch from '@/components/form/switch.vue';
+
+	const navBarChatIcon = computed(defaultStore.makeGetterSetter('navBarChatIcon'));
+	const navBarReloadIcon = computed(defaultStore.makeGetterSetter('navBarReloadIcon'));
+	const navBarWidgetIcon = computed(defaultStore.makeGetterSetter('navBarWidgetIcon'));
 </script>
