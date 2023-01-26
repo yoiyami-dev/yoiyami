1b3f370a1f23e5d96e45362edb66948f4740ff59
r-ca <nem@nem.x0.to>
2023-01-26 13:31:24 +0900

    メニュー作りかけ

---
+++ packages/client/src/pages/settings/beta-function.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="_formRoot">
+		<FormInfo class="_formBlock">以下の機能はすべてベータ版です。</FormInfo>
+
+		<FormSwitch v-model="$store.state.navBarChatIcon" class="_formBlock">チャットを表示する</FormSwitch>
+		<FormSwitch v-model="$store.state.navBarReloadIcon" class="_formBlock">リロードを表示する</FormSwitch>
+		<FormSwitch v-model="$store.state.navBarWidgetIcon" class="_formBlock">ウィジェットを表示する</FormSwitch>
+	</div>
+</template>
+	
+<script lang="ts" setup>
+	import FormInfo from '@/components/MkInfo.vue';
+	import FormSwitch from '@/components/form/switch.vue';
+</script>
