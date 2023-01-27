76933903380444d223e2b01be44de3e94bb7b7f3
r-ca <nem@nem.x0.to>
2023-01-27 18:55:26 +0900

    リファクタリング

--- packages/client/src/scripts/check-stream-mode.ts
+++ packages/client/src/scripts/check-stream-mode.ts
@@ -1,16 +1,12 @@
 
 export function checkStreamMode(note: Record<string, any>, me: Record<string, any> | null | undefined, enabled: boolean): boolean {
 	// 自分自身
-	// if (me && (note.userId === me.id)) return false;
+	if (me && (note.userId === me.id)) return false;
 
 	if (enabled && (note.visibility === 'followers' || note.visibility === 'specified')){
 		return true;
-		console.log('true');
 	}
 	else {
 		return false;
-		console.log('false');
 	}
-
-
 }
