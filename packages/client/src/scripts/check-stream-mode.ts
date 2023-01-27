7db2f0766dffefaf88c73ea3f4b82f2595ca8327
r-ca <nem@nem.x0.to>
2023-01-27 17:41:45 +0900

    ウィジェット直したり条件式修正した

--- packages/client/src/scripts/check-stream-mode.ts
+++ packages/client/src/scripts/check-stream-mode.ts
@@ -3,7 +3,9 @@ export function checkStreamMode(note: Record<string, any>, me: Record<string, an
 	// 自分自身
 	if (me && (note.userId === me.id)) return false;
 
-	if (note.visibility === 'followers' || note.visibility === 'specified') return true;
+	if (enabled && (note.visibility === 'followers' || note.visibility === 'specified')){
+		return true;
+	}
 	else {
 		return false;
 	}
--- packages/client/src/widgets/stream-mode.vue
+++ packages/client/src/widgets/stream-mode.vue
@@ -5,8 +5,35 @@
 	import { computed, ref, watch } from 'vue';
 	import { defaultStore } from '@/store';
 	import FormSwitch from '@/components/form/switch.vue';
+	import { GetFormResultType } from '@/scripts/form';
+	import { useWidgetPropsManager, Widget, WidgetComponentEmits, WidgetComponentExpose, WidgetComponentProps } from './widget';
 
 	const streamModeEnabled = computed(defaultStore.makeGetterSetter('streamModeEnabled'));
+
+	const name = 'postForm';
+
+	const widgetPropsDef = {
+	};
+
+	type WidgetProps = GetFormResultType<typeof widgetPropsDef>;
+
+	// 現時点ではvueの制限によりimportしたtypeをジェネリックに渡せない
+	//const props = defineProps<WidgetComponentProps<WidgetProps>>();
+	//const emit = defineEmits<WidgetComponentEmits<WidgetProps>>();
+	const props = defineProps<{ widget?: Widget<WidgetProps>; }>();
+	const emit = defineEmits<{ (ev: 'updateProps', props: WidgetProps); }>();
+
+	const { widgetProps, configure } = useWidgetPropsManager(name,
+		widgetPropsDef,
+		props,
+		emit,
+	);
+
+	defineExpose<WidgetComponentExpose>({
+		name,
+		configure,
+		id: props.widget ? props.widget.id : null,
+	});
 
 	//TODO: ちゃんとi18n対応しやがれ(@自分)
 </script>
