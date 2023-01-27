cd405ffabda2ef07cac56010bfae3e9c14f8304a
r-ca <nem@nem.x0.to>
2023-01-27 17:31:41 +0900

    ちゃんとfalse返すようにしてみた

--- packages/client/src/scripts/check-stream-mode.ts
+++ packages/client/src/scripts/check-stream-mode.ts
@@ -1,10 +1,12 @@
-import followersVue from "@/pages/user/followers.vue";
 
 export function checkStreamMode(note: Record<string, any>, me: Record<string, any> | null | undefined, enabled: boolean): boolean {
 	// 自分自身
 	if (me && (note.userId === me.id)) return false;
 
-	if (note.visibility === 'followers') return true;
-	if (note.visibility === 'specified') return true;
+	if (note.visibility === 'followers' || note.visibility === 'specified') return true;
+	else {
+		return false;
+	}
+
 
 }
