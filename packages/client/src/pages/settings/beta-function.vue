b2d4cc1992d4debe11f81359977fe6bc7094828e
r-ca <nem@nem.x0.to>
2023-02-06 00:43:05 +0900

    切り替えれるようにした

--- packages/client/src/pages/settings/beta-function.vue
+++ packages/client/src/pages/settings/beta-function.vue
@@ -5,6 +5,8 @@
 		<FormSwitch v-model="navBarChatIcon" class="_formBlock">チャットを表示する</FormSwitch>
 		<FormSwitch v-model="navBarReloadIcon" class="_formBlock">リロードを表示する</FormSwitch>
 		<FormSwitch v-model="navBarWidgetIcon" class="_formBlock">ウィジェットを表示する</FormSwitch>
+		<FormSwitch v-model="newVideoPlayerEnabled" class="_formBlock">新しいビデオプレイヤー(vue-plyr)を使用する</FormSwitch>
+
 	</div>
 </template>
 	
@@ -17,4 +19,5 @@ import { defaultStore } from '@/store';
 	const navBarChatIcon = computed(defaultStore.makeGetterSetter('navBarChatIcon'));
 	const navBarReloadIcon = computed(defaultStore.makeGetterSetter('navBarReloadIcon'));
 	const navBarWidgetIcon = computed(defaultStore.makeGetterSetter('navBarWidgetIcon'));
+	const newVideoPlayerEnabled = computed(defaultStore.makeGetterSetter('newVideoPlayerEnabled'));
 </script>
